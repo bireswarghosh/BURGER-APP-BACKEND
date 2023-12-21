34e5fc385a2c8f80eb8f29900f735669f2e9170f
bireswarghosh <116499672+bireswarghosh@users.noreply.github.com>
2023-12-22 00:40:36 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -58,8 +58,8 @@ router.get(
 
   passport.authenticate("google"),
   (req, res, next) => {
-    // res.redirect("/api/v1/me");
-    res.redirect(process.env.FRONTEND_URL);
+    res.redirect("/api/v1/me");
+    // res.redirect(process.env.FRONTEND_URL);
     // res.send("<h1>LOG IN </h1>")
   }
 );
